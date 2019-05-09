0e94f6816d2b790f0b93803eb5c2ee7de71210d5
somermaret <somermaret@gmail.com>
2019-05-09 15:39:35 +0300

    initial commit

--- src/main/java/Peaklass.java
+++ src/main/java/Peaklass.java
@@ -1,11 +1,25 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Peaklass extends Application {
 
     @Override
-    public void start(Stage Pealava) {
+    public void start(Stage pealava) {
+        BorderPane juur = new BorderPane();
 
+        // tekstivälja loomine ja lisamine piiripaanile (üles)
+        TextField tekst = new TextField();
+        tekst.setText("Tere! Mina olen tõenäosusteooria valemite kalkulaator.");
+        juur.setTop(tekst);
+
+
+        Scene stseen = new Scene(juur, 500, 500);
+        pealava.setTitle("Ülesanne 6");
+        pealava.setScene(stseen);
+        pealava.show();
     }
 
     public static void main(String[] args) {
